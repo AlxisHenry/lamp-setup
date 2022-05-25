354fdb025984dbd07e31220c0ab5ad9ce10b95eb
AlxisHenry <alexis.henry150357@gmail.com>
2022-05-25 10:09:41 +0200

    Fix: wrong a2ensite

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,15 +9,22 @@ sudo mv /home/ubuntu/linux-professional-environment/etc/ports.conf /etc/apache2/
 
 # Configs sites & configurations
 sudo mv /home/ubuntu/linux-professional-environment/etc/apache2/conf-available/phpmyadmin.conf /etc/apache2/conf-available/phpmyadmin.conf
-sudo mv /home/ubuntu/linux-professional-environment/etc/apache2/sites-available/main.conf /etc/apache2/sites-available/main.conf
-sudo a2ensite main.conf
+sudo mv /home/ubuntu/linux-professional-environment/etc/apache2/sites-available/laravel.conf /etc/apache2/sites-available/laravel.conf
+sudo a2ensite laravel.conf
+sudo a2dissite 000-default.conf
 sudo a2enconf phpmyadmin.conf
 
 # PhpMyAdmin
 sudo mv /home/ubuntu/linux-professional-environment/etc/phpmyadmin/config.inc.php /usr/share/phpmyadmin/config.inc.php
 
+# Apache Folders
+sudo rm -rf /var/www/html
+sudo mkdir /var/www/main
+
 # Rigths
 sudo chown -R www-data:www-data /usr/share/phpmyadmin
+sudo chown -R root:root /etc/apache2
+sudo chown -R ubuntu:ubuntu /var/www
 
 # Restart Apache
 sudo systemctl restart apache2
